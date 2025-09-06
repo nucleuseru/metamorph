05c1a9a55e495852f04705022da03c80b3d7a64c
nucleus48 <nucleuserumagborie@gmail.com>
2025-09-06 11:01:56 +0100

    initialize frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interSans = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Metamorph",
+  description:
+    "Metamorph lets you easily change faces and voices during live video calls.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en">
+      <body className={`${interSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
